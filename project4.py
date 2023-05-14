45fb5b29e692c27a983b76bd622a14511751c664
Yusuf <133169287+yusufuz0@users.noreply.github.com>
2023-05-14 17:33:32 +0300

    Delete project4.py

--- project4.py
+++
@@ -1,9 +0,0 @@
-# Kullanıcıdan ad,soyad ve numara bilgisini alarak bunları alt alta ekrana yazdırın.
-
-ad = input("adınız: ")
-soyad = input("soyadınız: ")
-numara = int(input("numaranız: "))
-
-print("\nBilgiler...\n")
-
-print("{} \n{} \n{}".format(ad,soyad,numara))
