1abaa63349ec1500c4217dfc1b052cf50720f7c7
Yusuf <133169287+yusufuz0@users.noreply.github.com>
2023-05-14 17:33:08 +0300

    Delete project1.py

--- project1.py
+++
@@ -1,7 +0,0 @@
-# Kullanıcıdan aldığınız 3 tane sayıyı çarparak ekrana yazdırın. Ekrana yazdırma işlemini *format* metoduyla yapmaya çalışın.
-
-sayi1 = int(input("birinci sayıyı  giriniz:"))
-sayi2 = int(input("ikinci sayıyı  giriniz:"))
-sayi3 = int(input("üçüncü sayıyı  giriniz:"))
-
-print("sayıların çarpımı: {}".format(sayi1*sayi2*sayi3))
