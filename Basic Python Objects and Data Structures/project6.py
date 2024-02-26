eb3fca59affb2b570a12f399ac26eef2a856ea7f
Yusuf <133169287+yusufuz0@users.noreply.github.com>
2024-02-26 19:02:00 +0300

    Delete Basic Python Objects and Data Structures directory

--- Basic Python Objects and Data Structures/project6.py
+++
@@ -1,8 +0,0 @@
-# Kullanıcıdan bir dik üçgenin dik olan iki kenarını(a,b) alın ve hipotenüs uzunluğunu bulmaya çalışın.
-# Hipotenüs Formülü: a^2 + b^2 = c^2
-
-kenar1 = int(input("kenar1 giriniz: "))
-kenar2 = int(input("kenar2 giriniz: "))
-
-
-print("\nhipotenüs: ",(kenar1**2 + kenar2**2)**0.5)
